2523521e9b78e108a18e85212b4436622510efcb
Ahmed Farag <farag0336@gmail.com>
2025-10-12 02:41:52 +0300

    add IP

--- book.network/src/main/java/com/farag/book/history/BookTransactionHistory.java
+++ book.network/src/main/java/com/farag/book/history/BookTransactionHistory.java
@@ -24,7 +24,7 @@ public class BookTransactionHistory extends BaseEntity {
 //    @ManyToOne
 //    @JoinColumn(name = "user_id")
 //    private User user;
-
+//
     @Column(name = "user_id")
     private String userId;
 
